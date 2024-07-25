f0e94f34b946affd3fc063908b9b1c9aa0a95e45
nishantbodhijani <nishantbodhijani@gmail.com>
2024-07-25 13:52:35 +0530

    Update api.js

--- src/Service/api.js
+++ src/Service/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://test.create.diagnal.com/data/';
 
+// Function to fetch data
 export const fetchPageData = (page) => {
   return axios.get(`${BASE_URL}page${page}.json`);
 };
